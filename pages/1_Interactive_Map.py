c6251443679fcd4fcf3846c1030cb6870636101f
Nilay Swarge <101919132+Nilay144@users.noreply.github.com>
2026-03-16 11:33:07 +0100

    Refactor interactive map with new basemap options

--- pages/1_Interactive_Map.py
+++ pages/1_Interactive_Map.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
 
+st.set_page_config(layout="wide")
+
 markdown = """
 Interactive map
 """
@@ -10,12 +12,13 @@ st.sidebar.info(markdown)
 logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
-
 st.title("Interactive Map")
 
-col1, col2 = st.columns([4, 1])
-options = list(leafmap.basemaps.keys())
-index = options.index("OpenTopoMap")
+# Basemap options
+basemap_options = list(leafmap.basemaps.keys())
+basemap_index = basemap_options.index("OpenTopoMap")
+
+basemap = st.sidebar.selectbox("Select a basemap:", basemap_options, basemap_index)
 
 # PDOK datasets
 pdok_datasets = {
@@ -23,52 +26,47 @@ pdok_datasets = {
         "url": "https://service.pdok.nl/hwh/luchtfotorgb/wms/v1_0?",
         "layers": "Actueel_orthoHR",
     },
-    "PDOK BGT Achtergrondkaart": {
-        "url": "https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0?",
-        "layer": "standaard",
-        "type": "wmts",
-    },
-    "PDOK Kadastrale Kaart": {
+    "Kadastralekaart": {
         "url": "https://service.pdok.nl/kadaster/kadastralekaart/wms/v5_0?",
         "layers": "perceel",
     },
-    "PDOK Bestuurlijke Grenzen": {
+    "Bestuurlijke Grenzen": {
         "url": "https://service.pdok.nl/kadaster/bestuurlijkegebieden/wms/v1_0?",
         "layers": "Gemeentegebied",
     },
 }
 
+selected_datasets = st.sidebar.multiselect(
+    "Select PDOK datasets:",
+    list(pdok_datasets.keys()),
+    default=["PDOK Luchtfoto RGB"]
+)
 
-with col2:
+# Create map
+m = leafmap.Map(
+    center=[52.1326, 5.2913],
+    zoom=7,
+    locate_control=True,
+    latlon_control=True,
+    draw_export=True,
+    minimap_control=True,
+)
 
-    basemap = st.selectbox("Select a basemap:", options, index)
+m.add_basemap(basemap)
 
-    selected_dataset = st.multiselect("Select PDOK datasets:", 
-                                      list(pdok_datasets.keys()),
-                                      default=["PDOK Luchtfoto RGB"])
-                                      
-with col1:
+for dataset in selected_datasets:
+    config = pdok_datasets[dataset]
 
-    m = leafmap.Map(
-        center=[52.1326, 5.2913],   # Netherlands
-        zoom=7,
-        locate_control=True,
-        latlon_control=True,
-        draw_export=True,
-        minimap_control=True,
-    )
-    m.add_basemap(basemap)
-
-    PDOK luchtfoto RGB WMS
     m.add_wms_layer(
-        url="https://service.pdok.nl/hwh/luchtfotorgb/wms/v1_0?",
-        layers="Actueel_orthoHR",
-        name="PDOK Luchtfoto RGB",
+        url=config["url"],
+        layers=config["layers"],
+        name=dataset,
         attribution="PDOK",
         transparent=True,
         format="image/png",
     )
 
-    # Optional layer control
-    m.add_layer_control()    
-    m.to_streamlit(height=700, width=1400)
+m.add_layer_control()
+
+# Full width map
+m.to_streamlit(height=700)
