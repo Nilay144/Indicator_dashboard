399fc0538cc4909c31b99565d510c39e170ec73d
Nilay Swarge <101919132+Nilay144@users.noreply.github.com>
2026-03-16 11:46:31 +0100

    Update 1_Interactive_Map.py

--- pages/1_Interactive_Map.py
+++ pages/1_Interactive_Map.py
@@ -9,42 +9,43 @@ Interactive map
 
 st.sidebar.title("About")
 st.sidebar.info(markdown)
-# logo = "https://i.imgur.com/UbOXYAU.png"
-# st.sidebar.image(logo)
+
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
 
 st.title("Interactive Map")
 
-# Basemap options
 basemap_options = list(leafmap.basemaps.keys())
 basemap_index = basemap_options.index("OpenTopoMap")
-
 basemap = st.sidebar.selectbox("Select a basemap:", basemap_options, basemap_index)
 
-# PDOK datasets
 pdok_datasets = {
     "PDOK Luchtfoto RGB": {
         "url": "https://service.pdok.nl/hwh/luchtfotorgb/wms/v1_0?",
         "layers": "Actueel_orthoHR",
+        "opacity": 1.0,
     },
-    "Landuse": {
-        "url": "https://service.pdok.nl/cbs/bestand-bodemgebruik-2017/wms/v1_0?request=getCapabilities&service=WMS",
-        "layers": "Landuse",
+    "Kadastralekaart": {
+        "url": "https://service.pdok.nl/kadaster/kadastralekaart/wms/v5_0?",
+        "layers": "perceel",
+        "opacity": 0.8,
     },
     "Bestuurlijke Grenzen": {
         "url": "https://service.pdok.nl/kadaster/bestuurlijkegebieden/wms/v1_0?",
         "layers": "Gemeentegebied",
+        "opacity": 0.7,
     },
 }
 
 selected_datasets = st.sidebar.multiselect(
     "Select PDOK datasets:",
-    list(pdok_datasets.keys())
+    list(pdok_datasets.keys()),
+    default=["PDOK Luchtfoto RGB", "Kadastralekaart", "Bestuurlijke Grenzen"]
 )
 
-# Create map
 m = leafmap.Map(
     center=[52.1326, 5.2913],
-    zoom=7,
+    zoom=8,
     locate_control=True,
     latlon_control=True,
     draw_export=True,
@@ -53,12 +54,27 @@ m = leafmap.Map(
 
 m.add_basemap(basemap)
 
-for dataset in selected_datasets:
-    config = pdok_datasets[dataset]
-
+# Add luchtfoto first as background
+if "PDOK Luchtfoto RGB" in selected_datasets:
+    cfg = pdok_datasets["PDOK Luchtfoto RGB"]
     m.add_wms_layer(
-        url=config["url"],
-        layers=config["layers"],
+        url=cfg["url"],
+        layers=cfg["layers"],
+        name="PDOK Luchtfoto RGB",
+        attribution="PDOK",
+        transparent=True,
+        format="image/png",
+    )
+
+# Add overlays after luchtfoto so they appear above it
+for dataset in selected_datasets:
+    if dataset == "PDOK Luchtfoto RGB":
+        continue
+
+    cfg = pdok_datasets[dataset]
+    m.add_wms_layer(
+        url=cfg["url"],
+        layers=cfg["layers"],
         name=dataset,
         attribution="PDOK",
         transparent=True,
@@ -66,6 +82,4 @@ for dataset in selected_datasets:
     )
 
 m.add_layer_control()
-
-# Full width map
-m.to_streamlit(height=700)
+m.to_streamlit(height=750)
